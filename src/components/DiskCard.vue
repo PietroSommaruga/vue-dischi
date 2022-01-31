866b34b9c20457bcee1f3867d06f6542ae8d4cff
PietroSommaruga <pietro.sommaruga2001@gmail.com>
2022-01-31 16:55:36 +0100

    fine esercizio

---
+++ src/components/DiskCard.vue
@@ -0,0 +1,25 @@
+<template>
+    
+        <div class=" text-center">
+        
+                <div class="disk-card pt-3 my-3">
+                    <img :src="disk.poster" alt="">
+                    <h6 class="pt-2">{{disk.title}}</h6>
+                    <span>{{disk.author}}</span>
+                    <p>{{disk.year}}</p>
+                </div>
+        
+        </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            disk: Object
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import '@/style/diskCard.scss';
+</style>
---
+++ src/components/MainContainer.vue
@@ -0,0 +1,26 @@
+<template>
+    <main>
+        <div class="container">
+            <div class="card-container">
+                <disk-card v-for="(disk, index) in discs" :key="index" :disk="disk"/>
+            </div>
+        </div>
+
+    </main>
+</template>
+
+<script>
+    import DiskCard from './DiskCard.vue'
+    export default {
+        components: {
+            DiskCard,
+        },
+        props: {
+            discs: Array
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import '@/style/mainContainer.scss';
+</style>

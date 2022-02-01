a2cf45fc2f2007a4170690b07a77201bc179b8cf
PietroSommaruga <pietro.sommaruga2001@gmail.com>
2022-02-01 16:31:22 +0100

    filtro per genere

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <header-content />
-      <main-container :discs="discs"/>
+    <header-content @filtro="filterGenre"/>
+      <main-container :discs="filteredDischi"/>
   </div>
 </template>
 
@@ -19,15 +19,32 @@ export default {
   data() {
     return {
       discs : [],
+      filteredDischi: [],
     }
   },
   mounted() {
     axios.get(' https://flynn.boolean.careers/exercises/api/array/music').then((result) => {
       this.discs = result.data.response
+      this.filteredDischi = result.data.response
+      
       
     })
+  },
+
+  methods: {
+    // ----- filtro per genere -----
+    filterGenre(selectedValue) {
+      if (selectedValue === 'All') {
+        this.filteredDischi = this.discs;
+      } else {
+        this.filteredDischi = this.discs.filter((disk) => {
+          return disk.genre.includes(selectedValue); 
+        });
+      }
+    },
   }
 }
+   
 </script>
 
 <style lang="scss">
--- src/components/HeaderContent.vue
+++ src/components/HeaderContent.vue
@@ -1,12 +1,29 @@
 <template>
     <header class="d-flex align-items-center">
         <img src="@/assets/logo-spotify.png" alt="">
+        <select v-model="selectedValue" @change="$emit('filtro', selectedValue)">
+                <option disabled value="">Scegli un genere</option>
+                <option value="All">All</option>
+                <option value="Jazz">Jazz</option>
+                <option value="Metal">Metal</option>
+                <option value="Pop">Pop</option>
+                <option value="Rock">Rock</option>
+            </select>
     </header>
 </template>
 
 <script>
     export default {
+        name: "Header",
+
+    data() {
+        return {
+            selectedValue: '',
+            selectedArtist: ''
+        }
     }
+   
+};
 </script>
 
 <style lang="scss" scoped>
